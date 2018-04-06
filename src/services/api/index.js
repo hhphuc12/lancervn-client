9c96ece3f207de86495710814af95438a8405d23
PHUC-PC\Phuc <tichuotuglyboy@gmail.com>
2018-04-06 17:19:14 +0700

    config upload image to s3

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -13,3 +13,8 @@ export const postRegister = (name, email, password) => {
     const url = `${API_URI}register`;
     return request.post(url, { name, email, password });
 };
+
+export const postImg = (img) => {
+    const url = `${API_URI}postImg`;
+    return request.post(url, img);
+};
