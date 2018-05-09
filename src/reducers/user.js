82d2416d6a99f4452fad0a6b4836565c26f2f46a
PHUC-PC\Phuc <tichuotuglyboy@gmail.com>
2018-05-09 11:36:10 +0700

    [+] freelancer detail at home screen

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -6,6 +6,9 @@ import {
     REQUEST_LIST_FREELANCER,
     RECEIVED_LIST_FREELANCER,
     ERROR_LIST_FREELANCER,
+    REQUEST_FREELANCER_DETAIL,
+    RECEIVED_FREELANCER_DETAIL,
+    ERROR_FREELANCER_DETAIL,
 } from "../constants/userType";
 import moment from "moment/moment";
 
@@ -13,6 +16,16 @@ const initialState = {
     userCategories: [],
     listFreelancer: [],
     isFetching: false,
+    avatarUri: '',
+    name: '',
+    province: '',
+    occupation: '',
+    description: '',
+    category: [],
+    experience: [],
+    projectDone: [],
+    literacy: [],
+    language:[],
 };
 
 const currentTime = moment().format();
@@ -60,6 +73,50 @@ export default function (
             };
 
         case ERROR_LIST_FREELANCER:
+            return {
+                ...state,
+                actionTime: action && action.time ?  action && action.time : currentTime,
+                isFetching:  action && action.isFetching ?  action && action.isFetching : initialState.isFetching,
+            };
+
+        case REQUEST_FREELANCER_DETAIL:
+            return {
+                ...state,
+                actionTime: action && action.time ?  action && action.time : currentTime,
+                isFetching:  action && action.isFetching ?  action && action.isFetching : initialState.isFetching
+            };
+
+        case RECEIVED_FREELANCER_DETAIL:
+            const freelancer = action && action.freelancer ? action && action.freelancer : initialState.freelancer;
+            const {
+                avatarUri,
+                name,
+                province,
+                occupation,
+                description,
+                category,
+                experience,
+                projectDone,
+                literacy,
+                language,
+            } = freelancer;
+            return {
+                ...state,
+                actionTime: action && action.time ?  action && action.time : currentTime,
+                isFetching: action && action.isFetching ?  action && action.isFetching : initialState.isFetching,
+                avatarUri,
+                name,
+                province,
+                occupation,
+                description,
+                category,
+                experience,
+                projectDone,
+                literacy,
+                language,
+            };
+
+        case ERROR_FREELANCER_DETAIL:
             return {
                 ...state,
                 actionTime: action && action.time ?  action && action.time : currentTime,
--- src/routes/HomeRoutes.js
+++ src/routes/HomeRoutes.js
@@ -9,14 +9,16 @@ import Home                     from '../containers/home/homePage';
 import User                     from '../containers/home/user';
 import Job                      from '../containers/home/job';
 import Package                  from '../containers/home/package';
+import UserDetail               from '../containers/home/user/detail';
 import PageNotFound             from '../containers/pageNotFound';
 
 const DashRoutes = () => (
     <Switch>
         <Route exact path='/' component={Home} />
-        <Route path='/freelancer' component={User} />
+        <Route path='/freelancers' component={User} />
         <Route path='/job-freelance' component={Job} />
         <Route path='/packages' component={Package} />
+        <Route path='/freelancer/:id' component={UserDetail} />
         <Route component={PageNotFound} />
     </Switch>
 );
